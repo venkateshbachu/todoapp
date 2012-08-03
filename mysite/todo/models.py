c7302e254e64cd641ce6eb149bb45453e71a480c
venkatesh <venkateshbachu@in.com>
2012-08-03 09:38:51 +0530

    changed configuration

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -5,6 +5,9 @@ class Todo(models.Model):
     todo = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
 
+    class Meta:     
+        ordering = ['id']
+
     def save(self,*args,**kwargs):
         super(Todo,self).save(*args,**kwargs)
         
